4c484f90db68c9f5fa6645b8195c6a10fb047a90
Kalyan Vemuri <vkalyan08499@gmail.com>
2021-05-05 15:40:41 +0530

    API_KEYS Removed

--- MapsTest/src/controller/Rev.java
+++ MapsTest/src/controller/Rev.java
@@ -12,7 +12,7 @@ public class Rev {
  
 	public static void main(String[] args) {
 		try {
-			URL url = new URL("https://apis.mapmyindia.com/advancedmaps/v1/r9qv315854csujrwdi98x3ft8ghldhp4/rev_geocode?lat=15.60&lng=79.61");
+			URL url = new URL("https://apis.mapmyindia.com/advancedmaps/v1/API_KEY/rev_geocode?lat=15.60&lng=79.61");
 			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
 			String strTemp = "";
 			while (null != (strTemp = br.readLine())) {
